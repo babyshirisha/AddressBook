d788bd7cecf632ddd9e04611f1c1a1f28977cb68
babyshirisha <babyshirisha24@gmail.com>
2022-07-08 14:20:06 +0530

    creating address book

---
+++ src/com/bl/adressbooksystem/AddressBookSystem.java
@@ -0,0 +1,9 @@
+package com.bl.adressbooksystem;
+
+public class AddressBookSystem {
+    public static void main(String[] args) {
+        System.out.println("welcome to address book program");
+        AddressBook addressBook = new AddressBook("Shirisha", "Kadali","Telengana", "Hyderabad", 500011, 9999999999l,"babyshirisha24@gmail.com","hyderabad");
+        System.out.println(addressBook);
+    }
+}
